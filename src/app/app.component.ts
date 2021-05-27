759df8d6ceff2c019164ded3303bd4ffe64f5781
Vtitanov <titanov.vm@mail.ru>
2021-05-27 17:42:34 +0300

    create base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
+import {Router} from "@angular/router"
+import {NbSidebarService} from "@nebular/theme";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'monitoring-tool';
+  constructor(private sidebarService: NbSidebarService,private router: Router) {
+  }
+  ngOnInit() {
+    this.router.navigate(['/login'])
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,69 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
+import {
+  NbSidebarModule,
+  NbButtonModule,
+  NbSidebarService,
+  NbListModule,
+  NbCardModule,
+  NbUserModule,
+  NbThemeService,
+  NbInputModule,
+  NbActionsModule,
+  NbCheckboxModule,
+  NbDatepickerModule, NbIconModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbContextMenuModule,
+  NbMenuModule,
+  NbAccordionModule,
+} from '@nebular/theme';
+
+import { FormsModule as ngFormsModule } from '@angular/forms';
+import { MainComponent } from './main/main.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+import { NgxChartsModule }from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    AppRoutingModule,
+    NbLayoutModule,
+    NbSidebarModule.forRoot(), // NbSidebarModule.forRoot(), //if this is your app.module
+    NbButtonModule,
+    NbListModule,
+    NbCardModule,
+    NbUserModule,
+    ngFormsModule,
+    NbInputModule,
+    NbActionsModule,
+    NbCheckboxModule,
+    NbRadioModule,
+    NbDatepickerModule,
+    NbSelectModule,
+    NbIconModule,
+    NbContextMenuModule,
+    NbMenuModule.forRoot(),
+    NbAccordionModule,
+    NgxChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
